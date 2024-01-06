8ed9d9ef79e0159b8969344581802e7cf9adcc3a
Paul K <spkim0921@gmail.com>
2024-01-06 12:36:51 -0800

    10th commit
    remove unnecessary requirements on model schema fields

--- models/Fixed.ts
+++ models/Fixed.ts
@@ -4,8 +4,8 @@ import mongoose from "mongoose"
 
 const FixedSchema = new mongoose.Schema({
     value: { type: Number, default: 0.00 },
-    title: { type: String, required: [true, 'title is required'], trim: true, maxlength: [80, 'title char limit is 80'] },
-    content: { type: String, required: [true, 'content is required'], maxlength: [10000, 'content char limit is 10000'] },
+    title: { type: String, trim: true, maxlength: [80, 'title char limit is 80'] },
+    content: { type: String, maxlength: [10000, 'content char limit is 10000'] },
     startDate: { type: Date, required: true, default: Date.now },
     endDate: { type: Date },
     planId: { type: Number, required: [true, 'planId is required'] },
--- models/Plan.ts
+++ models/Plan.ts
@@ -3,8 +3,8 @@
 import mongoose from "mongoose";
 
 const PlanSchema = new mongoose.Schema({
-    title: { type: String, required: [true, 'title is required'], trim: true, maxlength: [80, 'title char limit is 80'] },
-    content: { type: String, required: [true, 'content is required'], maxlength: [10000, 'content char limit is 10000'] },
+    title: { type: String, trim: true, maxlength: [80, 'title char limit is 80'] },
+    content: { type: String, maxlength: [10000, 'content char limit is 10000'] },
     createDate: { type: Date, required: true, default: Date.now },
     active: { type: Boolean, required: true, default: true },
     username: { type: String, required: [true, 'username is required'] },
